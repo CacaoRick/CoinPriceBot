3a825e1bf42daf40e0b8de58b453eeac77deb820
CacaoRick <cacaorick@gmail.com>
2017-09-24 16:21:17 +0800

    加入 /help。/history 現在可以在後面加上 m 或 y 來顯示月或年的歷史

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const config = require("./config")
 
 const bot = new Telegraf(config.botToken)
 
+bot.command("/help", (ctx) => {
+	switch (ctx.update.message.text.split(" ")[1]) {
+		default: {
+			ctx.replyWithMarkdown(`/price`)
+			break
+		}
+	}
+})
+
 bot.command("/price", (ctx) => {
 	axios.get("https://www.bitoex.com/api/v1/get_rate")
 		.then((res) => {
@@ -15,29 +24,72 @@ bot.command("/price", (ctx) => {
 })
 
 bot.command("/history", (ctx) => {
+	const params = ctx.update.message.text.split(" ")[1] ? ctx.update.message.text.split(" ")[1].toLowerCase() : null
 	axios.get("https://www.bitoex.com/charts/price_history")
 		.then((res) => {
-			const { data } = res
-			let month = 0
-			const date = _.map(data, (obj) => {
-				let newMonth = moment(obj.date, "x").format("M")
-				if (newMonth != month) {
-					month = newMonth
-					return month
-				} else {
-					return ""
-				}
-			})
-			const price = _.map(data, "price")
-
+			let { data } = res
 			const chart = quiche("line")
 			chart.setTitle("BitoEx history (NTD)")
-			chart.addData(price, null)
-			chart.addAxisLabels("x", date)
 			chart.setSparklines()
 			chart.setAutoScaling()
 			chart.setLegendHidden()
+			let prices, timeline
+			switch (params) {
+				case "year":
+				case "years":
+				case "y":
+				case "年": {
+					// 價格
+					prices = _.map(data, "price")
+					// 月份
+					let skipFirstMonth = true
+					let lastMonth = 0
+					timeline = _.map(data, (obj) => {
+						const month = moment(obj.date, "x").format("M")
+						if (month !== lastMonth) {
+							lastMonth = month
+							if (skipFirstMonth) {
+								skipFirstMonth = false
+								return ""
+							} else {
+								return lastMonth
+							}
+						} else {
+							return ""
+						}
+					})
+					break
+				}
+				case "month":
+				case "months":
+				case "m":
+				case "月":
+				default: {
+					// 取 30 天
+					data = _.takeRight(data, 30)
+					// 價格
+					prices = _.map(data, "price")
+					// 範圍
+					const sortedPrice = _.sortBy(prices)
+					const min = sortedPrice[0]
+					const max = _.last(sortedPrice)
+					chart.setAxisRange("y", Math.floor(min / 10000) * 10000, Math.ceil(max / 10000) * 10000, 10000)
+					// 日期
+					timeline = _.map(data, (obj) => {
+						const date = moment(obj.date, "x").format("D")
+						if (Number(date) % 5 === 0) {
+							return date
+						} else {
+							return ""
+						}
+					})
+					break
+				}
+			}
+			chart.addData(prices, null)
+			chart.addAxisLabels("x", timeline)
 
+			// 轉成圖片連結
 			const imageUrl = chart.getUrl(true)
 			ctx.replyWithPhoto(imageUrl)
 		})

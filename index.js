72505599791ef087dafb4b3f979ffa0fc5b5d719
CacaoRick <cacaorick@gmail.com>
2017-09-24 22:41:55 +0800

    更改command順序。把 currencies 獨立一個檔案

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const quiche = require("quiche")
 const moment = require("moment")
 const Telegraf = require("telegraf")
 const JsonDB = require("node-json-db");
-const config = require("./config")
+
 const api = require("./api")
+const config = require("./config")
+const supportCurrencies = require("./supportCurrencies")
 
 const db = new JsonDB("pools", true, true)
 const bot = new Telegraf(config.botToken)
@@ -32,62 +34,6 @@ bot.command("/help", (ctx) => {
 	}
 })
 
-bot.command("/setPool", (ctx) => {
-	const command = ctx.update.message.text.split(" ")
-	let poolapi = command[1]
-	const address = command[2]
-	if (poolapi && address) {
-		// 檢查 api 和 address
-		if (!poolapi.includes("://")) {
-			ctx.reply("礦池API位址錯誤")
-			return
-		}
-		if (!(address.startsWith("4") && address.length >= 95)) {
-			ctx.reply("錢包地址錯誤")
-			return
-		}
-
-		if (poolapi.includes("stats_address")) {
-			// 有 stats_address，先去掉
-			poolapi = poolapi.split("stats_address")[0]
-
-		} else if (!poolapi.endsWith("/")) {
-			// 沒 stats_address，也沒 / 結尾
-			poolapi = poolapi + "/"
-		}
-
-		const { username, id } = ctx.message.chat
-		const user = username ? username : id
-		poolapi = `${poolapi}stats_address?address=${address}&longpoll=false`
-
-		api.poolStats(poolapi)
-			.then((res) => {
-				ctx.replyWithMarkdown(res)
-				console.log(`user setpool ${poolapi}`)
-				db.push(`/${ctx.message.chat.id}`, poolapi);
-				ctx.reply("礦池設定已儲存，可使用 /poolStats 察看礦池統計")
-			})
-			.catch((err) => {
-				ctx.reply("請求失敗，礦池設定未儲存")
-				console.log(`setpool poolapi request error ${err}`)
-			})
-	} else {
-		ctx.reply("參數格式錯誤")
-	}
-})
-
-bot.command("/poolStats", (ctx) => {
-	const poolapi = db.getData(`/${ctx.message.chat.id}`)
-
-	api.poolStats(poolapi)
-		.then((res) => {
-			ctx.replyWithMarkdown(res)
-		})
-		.catch((err) => {
-			ctx.reply("API 請求失敗")
-			console.log(`poolapi request error ${err}`)
-		})
-})
 
 bot.command("/price", (ctx) => {
 	// 沒給幣別的話預設 btc
@@ -96,15 +42,15 @@ bot.command("/price", (ctx) => {
 
 	let promises = []
 
-	if (_.includes(bitoexCurrencies, currency)) {
+	if (_.includes(supportCurrencies.bitoex, currency)) {
 		promises.push(api.bitoex())
 	}
 
-	if (_.includes(bitfinexCurrencies, currency)) {
+	if (_.includes(supportCurrencies.bitfinex, currency)) {
 		promises.push(api.bitfinex(currency))
 	}
 
-	if (_.includes(bittrexCurrencies, currency)) {
+	if (_.includes(supportCurrencies.bittrex, currency)) {
 		promises.push(api.bittrex(currency))
 	}
 
@@ -195,6 +141,63 @@ bot.command("/history", (ctx) => {
 		})
 })
 
+bot.command("/poolStats", (ctx) => {
+	const poolapi = db.getData(`/${ctx.message.chat.id}`)
+
+	api.poolStats(poolapi)
+		.then((res) => {
+			ctx.replyWithMarkdown(res)
+		})
+		.catch((err) => {
+			ctx.reply("API 請求失敗")
+			console.log(`poolapi request error ${err}`)
+		})
+})
+
+bot.command("/setPool", (ctx) => {
+	const command = ctx.update.message.text.split(" ")
+	let poolapi = command[1]
+	const address = command[2]
+	if (poolapi && address) {
+		// 檢查 api 和 address
+		if (!poolapi.includes("://")) {
+			ctx.reply("礦池API位址錯誤")
+			return
+		}
+		if (!(address.startsWith("4") && address.length >= 95)) {
+			ctx.reply("錢包地址錯誤")
+			return
+		}
+
+		if (poolapi.includes("stats_address")) {
+			// 有 stats_address，先去掉
+			poolapi = poolapi.split("stats_address")[0]
+
+		} else if (!poolapi.endsWith("/")) {
+			// 沒 stats_address，也沒 / 結尾
+			poolapi = poolapi + "/"
+		}
+
+		const { username, id } = ctx.message.chat
+		const user = username ? username : id
+		poolapi = `${poolapi}stats_address?address=${address}&longpoll=false`
+
+		api.poolStats(poolapi)
+			.then((res) => {
+				ctx.replyWithMarkdown(res)
+				console.log(`user setpool ${poolapi}`)
+				db.push(`/${ctx.message.chat.id}`, poolapi);
+				ctx.reply("礦池設定已儲存，可使用 /poolStats 察看礦池統計")
+			})
+			.catch((err) => {
+				ctx.reply("請求失敗，礦池設定未儲存")
+				console.log(`setpool poolapi request error ${err}`)
+			})
+	} else {
+		ctx.reply("參數格式錯誤")
+	}
+})
+
 bot.startPolling()
 
 function getRange(array) {
@@ -208,67 +211,3 @@ function getRange(array) {
 		step,
 	}
 }
-
-const bitoexCurrencies = [
-	"BTC",
-]
-const bitfinexCurrencies = [
-	"BTC", "LTC", "ETH", "ETC", "RRT",
-	"ZEC", "XMR", "DSH", "BCC", "BCU",
-	"XRP", "IOT", "EOS", "SAN", "OMG",
-	"BCH", "NEO", "ETP",
-]
-const bittrexCurrencies = [
-	"LTC", "DOGE", "VTC", "PPC", "FTC",
-	"RDD", "NXT", "DASH", "POT", "BLK",
-	"EMC2", "XMY", "AUR", "EFL", "GLD",
-	"SLR", "PTC", "GRS", "NLG", "RBY",
-	"XWC", "MONA", "THC", "ENRG", "ERC",
-	"VRC", "CURE", "XMR", "CLOAK", "START",
-	"KORE", "XDN", "TRUST", "NAV", "XST",
-	"BTCD", "VIA", "UNO", "PINK", "IOC",
-	"CANN", "SYS", "NEOS", "DGB", "BURST",
-	"EXCL", "SWIFT", "DOPE", "BLOCK", "ABY",
-	"BYC", "XMG", "BLITZ", "BAY", "BTS",
-	"FAIR", "SPR", "VTR", "XRP", "GAME",
-	"COVAL", "NXS", "XCP", "BITB", "GEO",
-	"FLDC", "GRC", "FLO", "NBT", "MUE",
-	"XEM", "CLAM", "DMD", "GAM", "SPHR",
-	"OK", "SNRG", "PKB", "CPC", "AEON",
-	"ETH", "GCR", "TX", "BCY", "EXP",
-	"INFX", "OMNI", "AMP", "AGRS", "XLM",
-	"BTA", /*"BTC"*/, "CLUB", "VOX", "EMC",
-	"FCT", "MAID", "EGC", "SLS", "RADS",
-	"DCR", "SAFEX", "BSD", "XVG", "PIVX",
-	"XVC", "MEME", "STEEM", "2GIVE", "LSK",
-	"PDC", "BRK", "DGD", "DGD", "WAVES",
-	"RISE", "LBC", "SBD", "BRX", "DRACO",
-	"ETC", "ETC", "STRAT", "UNB", "SYNX",
-	"TRIG", "EBST", "VRM", "SEQ", "XAUR",
-	"SNGLS", "REP", "SHIFT", "ARDR", "XZC",
-	"NEO", "ZEC", "ZCL", "IOP", "DAR",
-	"GOLOS", "UBQ", "KMD", "GBG", "SIB",
-	"ION", "LMC", "QWARK", "CRW", "SWT",
-	"TIME", "MLN", "ARK", "DYN", "TKS",
-	"MUSIC", "DTB", "INCNT", "GBYTE", "GNT",
-	"NXC", "EDG", "LGD", "TRST", "GNT",
-	"REP", "ETH", "WINGS", "WINGS", "RLC",
-	"GNO", "GUP", "LUN", "GUP", "RLC",
-	"LUN", "SNGLS", "GNO", "APX", "TKN",
-	"TKN", "HMQ", "HMQ", "ANT", "TRST",
-	"ANT", "SC", "BAT", "BAT", "ZEN",
-	"1ST", "QRL", "1ST", "QRL", "CRB",
-	"CRB", "LGD", "PTOY", "PTOY", "MYST",
-	"MYST", "CFI", "CFI", "BNT", "BNT",
-	"NMR", "NMR", "TIME", "LTC", "XRP",
-	"SNT", "SNT", "DCT", "XEL", "MCO",
-	"MCO", "ADT", "ADT", "FUN", "FUN",
-	"PAY", "PAY", "MTL", "MTL", "STORJ",
-	"STORJ", "ADX", "ADX", "DASH", "SC",
-	"ZEC", "ZEC", "LTC", "ETC", "XRP",
-	"OMG", "OMG", "CVC", "CVC", "PART",
-	"QTUM", "QTUM", "XMR", "XEM", "XLM",
-	"NEO", "XMR", "DASH", "BCC", "BCC",
-	"BCC", "NEO", "WAVES", "STRAT", "DGB",
-	"FCT", "BTS", "OMG",
-]

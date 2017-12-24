7184655c329fc313b75d98fa285175381b942523
CacaoRick <cacaorick@gmail.com>
2017-12-24 21:29:59 +0800

    完成 Bittrex api

--- src/api/bittrex.js
+++ src/api/bittrex.js
@@ -1,14 +1,23 @@
-// 更新支援 market
-// https://bittrex.com/api/v1.1/public/getmarkets
-// map(results, (result) => {return result.MarketName})
-// [BTC-LTC, BTC-DOGE,...]
-// 或是抓 MarketName.BaseCurrency
-// [BTC, ETH, USDT...]
-
-// 抓價格
 // https://bittrex.com/api/v1.1/public/getticker?market=${market}
-// base要在前面 例如 BTC-LTC, USDT-BTC 不分大小寫
+// base 要在前面 例如 BTC-LTC, USDT-BTC 不分大小寫
 // 成功
 // {"success":true,"message":"","result":{"Bid":0.01924700,"Ask":0.01924704,"Last":0.01924704}}
 // 失敗
 // {"success":false,"message":"INVALID_MARKET","result":null}
+
+import axios from "axios"
+
+export default function (currency, base) {
+  base = base == null || base == "USD" ? "USDT" : base
+  return axios.get(`https://bittrex.com/api/v1.1/public/getticker?market=${base}-${currency}`)
+    .then((res) => {
+      const { result } = res.data
+      if (result) {
+        let price = base == "USDT" ? Number(data.last_price).toFixed(2) : data.last_price
+        return `*Bittrex* \`${result.Last}\` ${base}\n`
+      } else {
+        // 無結果
+        return ""
+      }
+    })
+}

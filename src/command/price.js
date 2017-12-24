fc10344b793b0cb7478851a56f85dbd1d14a000d
CacaoRick <cacaorick@gmail.com>
2017-12-24 20:51:47 +0800

    建立 /price

--- src/command/price.js
+++ src/command/price.js
@@ -1,3 +1,46 @@
-export default function (ctx) {
+import api from "../api"
 
+export default function (ctx) {
+  const command = ctx.message.text.split(" ")
+  const currency = command[1] ? command[1].toUpperCase() : "BTC"
+  const base = command[2] ? command[2].toUpperCase() : null
+
+  console.log(`/price ${currency} ${base}`)
+
+  // 送出 Loading...
+  ctx.reply("Loading...", {
+    reply_to_message_id: ctx.message.message_id,
+  })
+    .then((result) => {
+      const promises = []
+      let resultMessages = ""
+
+      // 多找 BitoEx
+      if (currency == "BTC") {
+
+      }
+
+      // 多找 MaiCoin
+      if (currency == "BTC" || currency == "ETH" || currency == "LTC") {
+
+      }
+
+      const binance = api.binance(currency, base)
+        .then((message) => {
+          if (message != "") {
+            resultMessages += message
+            return ctx.telegram.editMessageText(ctx.chat.id, result.message_id, null, resultMessages, {
+              parse_mode: "Markdown",
+            })
+          }
+        })
+      promises.push(binance)
+
+      Promise.all(promises)
+        .then(() => {
+          if (resultMessages == "") {
+            return ctx.telegram.editMessageText(ctx.chat.id, result.message_id, null, `${currency}-${base} 查無結果`)
+          }
+        })
+    })
 }

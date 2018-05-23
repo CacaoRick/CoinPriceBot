69a155150060a5d613e86584e215441b4bb3cd08
CacaoRick <cacaorick@gmail.com>
2018-05-23 13:08:51 +0800

    Revert "不用預設值"
    This reverts commit 83959b3f5710128d164220df572b06dfbb8f26f3.

--- src/db.js
+++ src/db.js
@@ -4,4 +4,11 @@ import FileSync from "lowdb/adapters/FileSync"
 const adapter = new FileSync("db.json")
 const db = lowdb(adapter)
 
+db.defaults({
+	users: {},
+	groups: {},
+	wsGroups: {},
+})
+	.write()
+
 export default db

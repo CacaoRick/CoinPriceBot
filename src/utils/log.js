c498019817f07a59bd574d1714c3b6ff9315d15d
CacaoRick <cacaorick@gmail.com>
2018-05-23 13:13:09 +0800

    完成 log

---
+++ src/utils/log.js
@@ -0,0 +1,11 @@
+import db from "../db"
+
+export default function log(ctx) {
+	if (ctx.chat.type === "private") {
+		const users = db.get("users")
+		users[ctx.chat.id] = users[ctx.chat.id] + 1
+	} else {
+		const groups = db.get("groups")
+		groups[ctx.chat.id] = groups[ctx.chat.id] + 1
+	}
+}

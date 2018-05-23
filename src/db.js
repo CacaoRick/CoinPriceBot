83959b3f5710128d164220df572b06dfbb8f26f3
CacaoRick <cacaorick@gmail.com>
2018-05-23 11:55:01 +0800

    不用預設值

--- src/db.js
+++ src/db.js
@@ -4,11 +4,4 @@ import FileSync from "lowdb/adapters/FileSync"
 const adapter = new FileSync("db.json")
 const db = lowdb(adapter)
 
-db.defaults({
-	users: {},
-	groups: {},
-	wsGroups: {},
-})
-	.write()
-
 export default db

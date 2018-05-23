20549259bfd7491b9948297d8ace464f0be019cd
CacaoRick <cacaorick@gmail.com>
2018-05-23 13:22:52 +0800

    修正 log

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,12 +1,8 @@
 import db from "../db"
 
 export default function log(ctx) {
-	if (ctx.chat.type === "private") {
-		const users = db.get("users")
-		users[ctx.chat.id] = users[ctx.chat.id] + 1
-	} else {
-		const groups = db.get("groups")
-		groups[ctx.chat.id] = groups[ctx.chat.id] + 1
-	}
-	db.write()
+	const target = ctx.chat.type === "private" ? "users" : "groups"
+	const count = db.get([target, ctx.chat.id]).value() || 0
+	db.set([target, ctx.chat.id], count + 1)
+		.write()
 }

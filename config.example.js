768c230de929348d8545f6f34412a4825ce1ae93
CacaoRick <cacaorick@gmail.com>
2018-05-24 09:28:43 +0800

    加入 ws error time 設定

--- config.example.js
+++ config.example.js
@@ -2,5 +2,6 @@ module.exports = {
 	botToken: "Your Bot Token",
 	botUsername: "Your Bot Username",
 	wsPriceUpdateTime: 10,	// seconds
+	wsPriceErrorTime: 180, 	// seconds
 	admin: "Your Chat ID",	// admin's chat id
 }

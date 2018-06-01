6d7062d47cc344510d17e86a86d5f0615dd25232
Rick Liao <cacaorick@gmail.com>
2018-06-01 23:23:14 +0800

    更改順序

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 module.exports = {
+	admin: "Your Chat ID",	// admin's chat id
 	botToken: "Your Bot Token",
 	botUsername: "Your Bot Username",
 	wsPriceUpdateTime: 10,	// seconds
 	wsPriceErrorTime: 180, 	// seconds
-	admin: "Your Chat ID",	// admin's chat id
 }

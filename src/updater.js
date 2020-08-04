7daea76986a4dcabff72542244cd8e73a7aca654
Rick Liao <cacaorick@gmail.com>
2020-08-05 02:00:25 +0800

    show 24h change in pin message

--- src/updater.js
+++ src/updater.js
@@ -11,7 +11,7 @@ let symbols = ['tBTCUSD', 'tETHUSD', 'fUSD']
 
 export async function start () {
   update()
-  setInterval(update, 10 * 1000)
+  setInterval(update, 15 * 1000)
 }
 
 async function update () {
@@ -22,6 +22,8 @@ async function update () {
   try {
     const priceMessages = []
     let rateMessage = ''
+    const priceChangeMessages = []
+    let rateChangeMessage = ''
 
     const results = await bitfinex.lastPrices(symbols.join(','))
 
@@ -29,32 +31,47 @@ async function update () {
     // https://exchange-docs.crypto.com/#public-get-ticker
     const response = await axios.get('https://api.crypto.com/v2/public/get-ticker?instrument_name=CRO_USDT')
     const price = _.get(response, 'data.result.data.a') // last price
+    const changePrice = _.get(response, 'data.result.data.c', 0) // 24h change
     const croPrice = price.toFixed(4)
+    const croChange = (changePrice > 0 ? '+' : '') + (100 * changePrice / (croPrice - changePrice)).toFixed(2)
     const croMessage = `CRO \`${croPrice}\``
+    const croChangeMessage = `CRO \`${croChange}%\``
 
     // 準備訊息
     results.forEach((result) => {
       if (result.length === 11) {
+        const currency = result[0].replace('t', '').replace('USD', '')
         const price = result[7]
+        const dailyChange = result[6] > 0 ? '+' : '' + (100 * result[6]).toFixed(2)
         // 計算小數後位數
         const factoryDigital = 5 - price.toFixed(0).length
-        priceMessages.push(`${result[0].replace('t', '').replace('USD', '')} \`${price.toFixed(factoryDigital)}\``)
+        priceMessages.push(`${currency} \`${price.toFixed(factoryDigital)}\``)
+        priceChangeMessages.push(`${currency} \`${dailyChange}%\``)
       } else {
         // fUSD 最後利率
-        const rate = result[10]
-        rateMessage = `% \`${(100 * rate).toFixed(4)}\``
+        const rate = (100 * rate).toFixed(4)
+        const dailyChange = result[9] > 0 ? '+' : '' + (100 * result[9]).toFixed(2)
+        rateMessage = `f$ \`${rate}%\``
+        rateChangeMessage = `f$ \`${dailyChange}%\``
       }
     })
 
     // 更新訊息
     _.forEach(db.main.value(), async (group, groupId) => {
       const newMessage = [rateMessage, ...priceMessages, croMessage].join(' |\n')
+      const newChangeMessage = [rateChangeMessage, ...priceChangeMessages, croChangeMessage].join(' |\n')
       if (lastMessage[groupId] !== newMessage) {
         lastMessage[groupId] = newMessage
         await bot.editMessageText(newMessage, {
           parse_mode: 'Markdown',
           ...group.priceMessage,
         })
+        setTimeout(() => {
+          await bot.editMessageText(newChangeMessage, {
+            parse_mode: 'Markdown',
+            ...group.priceMessage,
+          })
+        }, 10000)
       }
 
       await bot.editMessageText(

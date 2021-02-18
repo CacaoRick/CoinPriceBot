6bce1edd9ea0cbab455811977b344b3c1122e042
Rick Liao <cacaorick@gmail.com>
2021-02-18 14:02:27 +0800

    response ping, auto reconnect ws.

--- src/libs/binanceFundingFeeWs.js
+++ src/libs/binanceFundingFeeWs.js
@@ -1,28 +1,11 @@
 import db from 'libs/db'
 import WebSocket from 'ws'
 
-const ws = new WebSocket('wss://fstream.binance.com/stream')
-
-ws.on('message', (message) => {
-  try {
-    const result = JSON.parse(message)
-    if (result.data) {
-      const fundingFeeData = result.data.map(data => {
-        return {
-          symbol: data.s,
-          fundingFeeRate: 100 * Number(data.r),
-          updatedAt: new Date(data.E),
-          timeToNextFunding: new Date(data.T),
-        }
-      })
-      db.fundingFee.set('fundingFees', fundingFeeData).write()
-    }
-  } catch (error) {
-    console.log('ws message parse error', error)
-  }
-})
+const reconnectInterval = 60e3
 
 function start () {
+  const ws = new WebSocket('wss://fstream.binance.com/stream')
+  
   ws.on('open', () => {
     console.log('Binance ws open')
     ws.send(JSON.stringify({
@@ -30,6 +13,32 @@ function start () {
       params: ['!markPrice@arr'],
       id: 1,
     }))
+  })
+  ws.on('error', (error) => {
+    console.log('socket error', error.message)
+  })
+  ws.on('close', () => {
+    console.log('socket close')
+    setTimeout(start, reconnectInterval)
+  })
+  ws.on('ping', () => ws.pong())
+  ws.on('message', (message) => {
+    try {
+      const result = JSON.parse(message)
+      if (result.data) {
+        const fundingFeeData = result.data.map(data => {
+          return {
+            symbol: data.s,
+            fundingFeeRate: 100 * Number(data.r),
+            updatedAt: new Date(data.E),
+            timeToNextFunding: new Date(data.T),
+          }
+        })
+        db.fundingFee.set('fundingFees', fundingFeeData).write()
+      }
+    } catch (error) {
+      console.log('ws message parse error', error)
+    }
   })
 }
 

cbe3828af66a44bd066de546125abc4e0d532999
Rick Liao <cacaorick@gmail.com>
2021-02-23 19:55:18 +0800

    db path

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,8 +1,8 @@
 import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
 
-const main = low(new FileSync('db.json'))
-const fundingFee = low(new FileSync('funding-fee.json'))
+const main = low(new FileSync('data/db.json'))
+const fundingFee = low(new FileSync('data/funding-fee.json'))
 
 export default {
   main,
